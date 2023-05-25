699accd4aae04166b966b24276913f212ddac4dd
Сергей Паньшин <low02345@bk.ru>
2023-05-25 18:55:12 +0500

    feat: add grouping by year and author

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import Book from "./Book";
 import useBookSorting from "../hooks/useBookSorting";
+import Button from "./UI/button/Button";
 
 function BookList({ books, title, remove }) {
-  const { groupedBooks, years, randomRecommendedBook } = useBookSorting(books);
+  const [groupedBy, setGroupedBy] = useState('years');
+
+  const { groupedBooksByYears,
+    years,
+    groupedBooksByRating,
+    rating,
+    groupedBooksByAuthors,
+    authors,
+    randomRecommendedBook } = useBookSorting(books);
+
+  function checkGroupedBy() {
+    if (groupedBy === 'authors') return [authors, groupedBooksByAuthors] ;
+    if (groupedBy === 'rating') return [rating, groupedBooksByRating];
+    return [years, groupedBooksByYears];
+  }
+  const groupName = checkGroupedBy();
 
   return (
     <div className="page__book-list">
+      <h3 className="page__group-title">Группировать книги:</h3>
+      <div className="page__sort-buttons">
+        <Button onClick={() => setGroupedBy('rating')} >По рейтингу</Button>
+        <Button onClick={() => setGroupedBy('authors')} >По автору</Button>
+        <Button onClick={() => setGroupedBy('years')} >По году</Button>
+      </div>
       <h1 className="page__title">{title}</h1>
       {randomRecommendedBook &&
       (
@@ -24,10 +47,10 @@ function BookList({ books, title, remove }) {
         </div>
       )
       }
-      {years.map((year) => (
+      {groupName[0].map((year) => (
         <div key={year}>
           <h2 className="page__group-title">{year}</h2>
-          {groupedBooks[year].map((book) => (
+          {groupName[1][year].map((book) => (
             <Book
               remove={remove}
               book={book}
--- src/hooks/useBookSorting.jsx
+++ src/hooks/useBookSorting.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 
 function useBookSorting(books) {
-  const [groupedBooks, setGroupedBooks] = useState({});
+  const [groupedBooksByYears, setGroupedBooksByYears] = useState({});
+  const [groupedBooksByRating, setGroupedBooksByRating] = useState({});
+  const [groupedBooksByAuthors, setGroupedBooksByAuthors] = useState({});
   const [years, setYears] = useState([]);
+  const [rating, setRating] = useState([]);
+  const [authors, setAuthors] = useState([]);
   const [randomRecommendedBook, setRandomRecommendedBook] = useState(null);
 
   useEffect(() => {
@@ -10,21 +14,47 @@ function useBookSorting(books) {
       a.title.toLowerCase().localeCompare(b.title.toLowerCase())
     );
 
-    const grouped = sorted.reduce((grouped, book) => {
+    // Группировка по годам
+    const groupedByYear = sorted.reduce((grouped, book) => {
       const year = book.year || "Книги без указания года";
       grouped[year] = grouped[year] || [];
       grouped[year].push(book);
       return grouped;
     }, {});
-    setGroupedBooks(grouped);
+    setGroupedBooksByYears(groupedByYear);
 
-    const sortedYears = Object.keys(grouped).sort((a, b) => {
+    const sortedYears = Object.keys(groupedByYear).sort((a, b) => {
       if (a === "Книги без указания года") return 1;
       if (b === "Книги без указания года") return -1;
       return parseInt(b) - parseInt(a);
     });
     setYears(sortedYears);
 
+    // Группировка по рейтингу
+    const groupedByRating = sorted.reduce((grouped, book) => {
+      const rating = +book.rating;
+      grouped[rating] = grouped[rating] || [];
+      grouped[rating].push(book);
+      return grouped;
+    }, {});
+    setGroupedBooksByRating(groupedByRating);
+
+    const sortedRating = Object.keys(groupedByRating).sort((a,b) => b - a);
+    setRating(sortedRating);
+
+    // Группировка по авторам
+    const groupedByAuthors = sorted.reduce((grouped, book) => {
+      const authors = book.authors;
+      grouped[authors] = grouped[authors] || [];
+      grouped[authors].push(book);
+      return grouped;
+    }, {})
+    setGroupedBooksByAuthors(groupedByAuthors);
+
+    const sortedAuthors = Object.keys(groupedByAuthors).sort((a, b) => a.trim().toLocaleLowerCase().localeCompare(b.trim().toLocaleLowerCase()));
+    setAuthors(sortedAuthors);
+
+    // Сортировка рекомендуемой книги
     const timeTestedBooks = sorted.filter(
       (book) => book.year !== "" && new Date().getFullYear() - book.year >= 3
     );
@@ -36,7 +66,7 @@ function useBookSorting(books) {
     setRandomRecommendedBook(randomBook);
   }, [books]);
 
-  return { groupedBooks, years, randomRecommendedBook };
+  return { groupedBooksByYears, years, groupedBooksByRating, rating, groupedBooksByAuthors, authors, randomRecommendedBook };
 }
 
 export default useBookSorting;
